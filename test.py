fa1805649828367cc053d757a98ebce7d91f3e78
lakshmivenkateshwaran <vvenkivasudevan9@gmail.com>
2022-05-26 14:27:44 +0530

    test

---
+++ test.py
@@ -0,0 +1,9 @@
+import pandas as pd  
+  
+# string values in the list   
+lst = ['Java', 'Python', 'C', 'C++',  
+         'JavaScript', 'Swift', 'Go']  
+  
+# Calling DataFrame constructor on list  
+dframe = pd.DataFrame(lst)  
+print(dframe)  
